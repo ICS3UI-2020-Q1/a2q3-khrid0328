316c75aecd8f7b476811572bb0401353b73259e3
khrid0328 <khrid0328@wrdsb.ca>
2020-09-15 15:07:33 +0000

    A2Q3 Practice questions

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ * The program will ask for two integers, then will output them in ascending order
+ * @author Dafna Khripun
  */
 public class Main {
 
@@ -9,7 +11,27 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // create a Scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // ask the user for the first integer
+    System.out.println("Please enter an integer:");
+
+    // declare and initialize first integer
+    int integer1 = input.nextInt();
+
+    // ask the user for the second integer
+    System.out.println("Please enter another integer:");
+
+    // declare and initialize second integer
+    int integer2 = input.nextInt();
+
+    // find the biggest number
+    if(integer1 < integer2){
+      System.out.println("Your numbers in ascending order are " + integer1 + ", " + integer2);
+    } else {
+      System.out.println("Your numbers in ascending order are " + integer2 + ", " + integer1);
+    }
+  
   }
 }
